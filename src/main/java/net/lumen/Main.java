c3bf187dd7ab3e633f81b719faeb4ddc9d9d186e
orang3i <orangeinfotainment@gmail.com>
2025-02-23 23:04:14 +0530

    server impl

---
+++ src/main/java/net/lumen/Main.java
@@ -0,0 +1,37 @@
+package net.lumen;
+
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        MinecraftServer server = MinecraftServer.init();
+
+
+        InstanceManager instanceManager = server.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 50, 0));
+        });
+
+        server.start("localhost", 25565);
+
+    }
+
+}
