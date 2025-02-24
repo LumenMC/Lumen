80a70c8d5027e7427a4d40db1e506c4fc574646f
orang3i <orangeinfotainment@gmail.com>
2025-02-24 09:58:03 +0530

    server impl - add lighting

--- src/main/java/net/lumen/Main.java
+++ src/main/java/net/lumen/Main.java
@@ -5,12 +5,18 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.instance.Chunk;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.item.ItemStack;
-import net.minestom.server.item.Material;
+
+
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
 public class Main{
 
@@ -22,6 +28,19 @@ public class Main{
         InstanceManager instanceManager = server.getInstanceManager();
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
         instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+        instanceContainer.setChunkSupplier(LightingChunk::new);
+
+
+        var chunks = new ArrayList<CompletableFuture<Chunk>>();
+        forChunksInRange(0, 0, 32, (x, z) -> chunks.add(instanceContainer.loadChunk(x, z)));
+
+        CompletableFuture.runAsync(() -> {
+            CompletableFuture.allOf(chunks.toArray(CompletableFuture[]::new)).join();
+            System.out.println("load end");
+            LightingChunk.relight(instanceContainer, instanceContainer.getChunks());
+            System.out.println("light end");
+        });
+
 
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
@@ -30,8 +49,19 @@ public class Main{
             player.setRespawnPoint(new Pos(0, 50, 0));
         });
 
+
         server.start("localhost", 25565);
 
+    }
+
+
+
+    public static void forChunksInRange(int centerX, int centerZ, int radius, BiConsumer<Integer, Integer> action) {
+        for (int x = centerX - radius; x <= centerX + radius; x++) {
+            for (int z = centerZ - radius; z <= centerZ + radius; z++) {
+                action.accept(x, z);
+            }
+        }
     }
 
 }
