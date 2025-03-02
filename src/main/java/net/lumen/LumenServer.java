81a1d7b8bd207b2bf8f165366c56d7a8e84d8cda
orang3i <orangeinfotainment@gmail.com>
2025-03-02 09:55:03 +0530

    Remove unwanted Getters

--- src/main/java/net/lumen/LumenServer.java
+++ src/main/java/net/lumen/LumenServer.java
@@ -19,7 +19,7 @@ public class LumenServer {
         MinecraftServer server = new MinecraftServer();
 
 
-        String address = "localhost"; //can be altered in config
+        String address = "localhost"; //can be altered in config yes
         int port = 25565;
 
         MinecraftServer.init();
